1efa3efe1f004285a9137ea66f37f0286a142acb
Hervé Le Meur CB <hlemeur@cloudbees.com>
2026-08-06 13:16:56 +0200

    cleanup

--- test/github-test.js
+++
@@ -1,9 +0,0 @@
-import assert from "assert";
-import github from "../lib/github.js";
-
-describe("The GitHub helpers", function () {
-  it("FIXME - make tests", function () {
-    assert.ok(github.commitExists);
-    assert.ok(github.createStatus);
-  });
-});
